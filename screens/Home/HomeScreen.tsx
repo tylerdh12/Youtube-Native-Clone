16c183cb6664f3edf9f2b284de6d5e8b45070b8d
Tyler Harper <tylerdh12@gmail.com>
2020-09-15 13:01:19 -0700

    Seperate Data

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -4,63 +4,7 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import SuggestedCategories from "../../components/SuggestedCategories";
 import { View } from "../../components/Themed";
 import VideoList from "./VideoList";
-
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53ab28ba",
-    title: "First Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91a97f63",
-    title: "Second Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-14557129d72",
-    title: "Third Item",
-    creator: "Creator Name",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-    thubmnail: "https://reactnative.dev/img/tiny_logo.png",
-    stats: "6.4M views 4 days ago",
-    duration: "6:37",
-  },
-];
+const { DATA } = require("../../DATA");
 
 const HomeScreen = () => {
   return (
