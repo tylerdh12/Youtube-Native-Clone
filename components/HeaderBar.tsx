23a6c22473b66434eddd241cd26c8f356f896126
Tyler Harper <tylerdh12@gmail.com>
2020-09-15 18:00:28 -0700

    Change HeaderBar to Functional Comp

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -4,110 +4,109 @@ import { Text } from "react-native";
 import { Avatar } from "react-native-elements";
 import { View } from "./Themed";
 
-export class HeaderBar extends React.Component {
-  constructor(props: Readonly<{}>) {
-    super(props);
-  }
-
-  render() {
-    return (
+const HeaderBar = () => {
+  return (
+    <View
+      style={{
+        height: 100,
+        width: "100%",
+        backgroundColor: "#292929",
+        position: "absolute",
+        justifyContent: "space-between",
+        display: "flex",
+        flexDirection: "row",
+        borderBottomColor: "#555555",
+        borderBottomWidth: 1,
+      }}
+    >
       <View
         style={{
-          height: 100,
+          marginTop: 50,
+          height: 40,
           width: "100%",
-          backgroundColor: "#292929",
+          backgroundColor: "transparent",
           position: "absolute",
           justifyContent: "space-between",
           display: "flex",
           flexDirection: "row",
-          borderBottomColor: "#555555",
-          borderBottomWidth: 1,
         }}
       >
         <View
           style={{
-            marginTop: 50,
-            height: 40,
-            width: "100%",
-            backgroundColor: "transparent",
-            position: "absolute",
-            justifyContent: "space-between",
             display: "flex",
             flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: "transparent",
           }}
         >
-          <View
+          <Ionicons
+            name="logo-youtube"
+            size={35}
+            color="red"
             style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              backgroundColor: "transparent",
+              marginLeft: 8,
+              padding: 0,
+              alignSelf: "center",
+            }}
+          />
+          <Text
+            style={{
+              color: "white",
+              padding: 6,
+              fontWeight: "700",
             }}
           >
-            <Ionicons
-              name="logo-youtube"
-              size={35}
-              color="red"
-              style={{
-                marginLeft: 8,
-                padding: 0,
-                alignSelf: "center",
-              }}
-            />
-            <Text
-              style={{
-                color: "white",
-                padding: 6,
-                fontWeight: "700",
-              }}
-            >
-              YouTube
-            </Text>
-          </View>
-          <View
+            YouTube
+          </Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "transparent",
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: 8,
+          }}
+        >
+          <MaterialIcons
+            name="cast"
+            size={26}
+            color="lightgrey"
             style={{
-              display: "flex",
-              flexDirection: "row",
-              backgroundColor: "transparent",
-              alignItems: "center",
-              justifyContent: "center",
-              marginRight: 8,
+              padding: 10,
             }}
-          >
-            <MaterialIcons
-              name="cast"
-              size={26}
-              color="lightgrey"
-              style={{
-                padding: 10,
-              }}
-            />
-            <SimpleLineIcons
-              name="camrecorder"
-              size={26}
-              color="lightgrey"
-              style={{
-                padding: 10,
-              }}
-            />
-            <Ionicons
-              name="ios-search"
-              size={26}
-              color="lightgrey"
-              style={{
-                padding: 10,
-              }}
-            />
-            <Avatar
-              rounded
-              source={{
-                uri:
-                  "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-              }}
-            />
-          </View>
+          />
+          <SimpleLineIcons
+            name="camrecorder"
+            size={26}
+            color="lightgrey"
+            style={{
+              padding: 10,
+            }}
+          />
+          <Ionicons
+            name="ios-search"
+            size={26}
+            color="lightgrey"
+            style={{
+              padding: 10,
+            }}
+          />
+          <Avatar
+            rounded
+            source={{
+              uri:
+                "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+            }}
+            onPress={() => {
+              alert("User Account Pressed");
+            }}
+          />
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
+
+export default HeaderBar;
